93cede67cf09a1538983cb6b56e80bf1a89633ae
silviu.chirpac <s.silviu88@yahoo.com>
2017-07-07 02:14:27 -0700

    deleted   .idea

--- Collections/warmup-part2/src/main/java/exercise0/Exercise0.java
+++ Collections/warmup-part2/src/main/java/exercise0/Exercise0.java
@@ -10,12 +10,14 @@ public class Exercise0 {
 
     public Exercise0(){
 
+
     }
 
     public void iterateThroughMap(){
 
         // TODO Exercise #0 a) Create a Map (HashMap) and add elements to it (using put() method)
         // TODO Exercise #0 a) Hint: Don't forget to specify the types of the key and value when creating the Map
+
 
         // TODO Exercise #0 b) Iterate over the Map using keySet() method and print all its elements
         // TODO Exercise #0 b) The elements could be printed like this: [key1=value1, key2=value2, ...]
